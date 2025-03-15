1c07332c5e059e919006d121f2535c33a1211eed
Emekaagu <aguchukwuemekag@gmail.com>
2025-03-15 22:06:44 +0100

    Update Hero section text and button label for clarity and impact

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='w-full bg-center bg-cover bg-no-repeat h-[90vh] lg:h-[84vh] bg-hero'>
-        <div className='w-full px-3 md:px-8 lg:px-[96px] h-full grid md:grid-cols-2 items-center bg-black bg-opacity-40'>
+        <div className='w-full px-3 md:px-8 lg:px-[96px] h-full grid lg:grid-cols-2 items-center bg-black bg-opacity-40'>
             <div>
-                <h1 className='md:text-7xl text-5xl text-center md:text-left leading-[1] text-white font-bold font-ageo'>Helping <br/> Businesses <span className='text-[#3bc330]'>Grow</span>  <br/>in Africa.</h1>
+                <h1 className='md:text-7xl capitalize text-5xl text-center lg:text-left leading-[1] text-white font-bold font-ageo'>Enabling <br/> business growth <span className='text-[#3bc330]'>through</span> <br/> seamless payment collections.</h1>
 
-                <button className='font-sora text-white h-[65px] border border-white mx-auto md:mx-0 block rounded-lg w-[250px] font-[500] my-8 text-xl bg-[#fb5607]'>Contact Sale</button>
+                <button className='font-sora text-white h-[65px] border border-white mx-auto lg:mx-0 block rounded-lg w-[250px] font-[500] my-8 text-xl bg-[#fb5607]'>Contact Sales</button>
 
             </div>
 
